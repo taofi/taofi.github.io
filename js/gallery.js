63ea1b85047a345b4e33b8126eaaff56d0f79618
taofi <58530085+taofi@users.noreply.github.com>
2023-11-26 16:31:32 +0300

    new site add

---
+++ js/gallery.js
@@ -0,0 +1,27 @@
+const arrL = document.getElementById('arrowL').querySelector('img');
+const arrR = document.getElementById('arrowR').querySelector('img');
+const galleryWindow = document.getElementById('galleryWindow');
+const slides = document.getElementsByClassName('slide');
+const countSlide = slides.length;
+let visSlide = 0;
+let last = 0;
+slides[visSlide].style.opacity = '1';
+
+function Swipe(direction){
+    last = visSlide;
+    visSlide += direction;
+    if(visSlide < 0)
+        visSlide = countSlide - 1;
+    if(visSlide >= countSlide)
+        visSlide = 0;
+    change();
+}
+
+function change(){
+    console.log(visSlide);
+    slides[last].style.opacity ='0';
+    slides[visSlide].style.opacity = '1';
+}
+console.log(arrL);
+arrR.addEventListener('click', () => Swipe(1));
+arrL.addEventListener('click', () => Swipe(-1));
---
+++ js/nav.js
@@ -0,0 +1,26 @@
+const nav = document.getElementsByTagName('nav')[0];
+let currentNavHeight = nav.offsetHeight;
+
+window.addEventListener('resize', function(){
+    currentNavHeight = 70;
+    if(767 > window.innerWidth)
+        currentNavHeight = 50;
+    if(550 > window.innerWidth)
+        currentNavHeight = 35;
+    if(420 > window.innerWidth)
+        currentNavHeight = 25;
+    if(this.scrollY > 10)
+        nav.style.height = currentNavHeight / 1.5 + 'px';
+});
+
+window.addEventListener('scroll', function() {
+    if(this.scrollY > 10)
+    {
+        nav.style.backgroundColor = "#FB9D11";
+        nav.style.height = currentNavHeight / 1.5 + 'px';
+    }else
+    {
+        nav.style.removeProperty('height');
+        nav.style.backgroundColor = '';
+    }
+  });
